242916044455a7ec8b3e31136cbcf32b4935623e
Yassine Bridi <ybridi@gmail.com>
2021-05-21 14:09:08 +0100

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+All the work is done by: https://github.com/chebyrash/cors
+
+With some changes.
---
+++ index.js
@@ -0,0 +1,27 @@
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  try {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/")
+      return new Response(`
+        Usage:\n
+          ${url.origin}/<url>
+      `);
+    console.log(request.url.slice(url.origin.length + 1));
+    let response = await fetch(request.url.slice(url.origin.length + 1), {
+      method: request.method,
+      headers: request.headers,
+      redirect: "follow",
+      body: request.body,
+    });
+    response = new Response(response.body, response);
+    response.headers.set("Access-Control-Allow-Origin", "*");
+    return response;
+  } catch (e) {
+    return new Response(e.stack || e, { status: 500 });
+  }
+}
